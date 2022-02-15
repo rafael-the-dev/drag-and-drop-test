fef48e9c70fd641c47e1e1d49d6532cd369d6335
Rafael Tivane <luislangabusiness@gmail.com>
2022-02-15 17:23:25 +0200

    adding dragging specifications

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,13 +1,24 @@
 import './styles.css'
+import { forwardRef } from 'react'
+import { useDrag, useDrop } from 'react-dnd'
+import { ItemTypes } from '../../config'
 
-const Button = ({ label }) => {
+const Button = forwardRef(({ label }, ref) => {
+    const [ { isDragging }, drag ] = useDrag(() => ({
+        type: ItemTypes.BUTTON,
+        collect: (monitor) => ({
+          isDragging: !!monitor.isDragging()
+        })
+    }));
+
     return (
         <button
             className="py-4 px-8 mb-4 text-slate-100 button"
+            ref={drag}
             >
             { label }
         </button>
     ); 
-};
+});
 
 export default Button;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,20 @@
 import Button from '../../components/Button';
 import './styles.css'
+import { useDrag, useDrop } from 'react-dnd'
+import { ItemTypes } from '../../config'
 
 const Home = () => {
+    
+    const [, drop] = useDrop(
+        () => ({
+          accept: ItemTypes.BUTTON,
+          drop: (monitor) => {
+              console.log(monitor)
+          }
+        }),
+        []
+      )
+
     return (
         <main>
             <div className='flex flex-wrap px justify-between pt-8'>
@@ -13,8 +26,12 @@ const Home = () => {
                 )) }
             </div>
             <section className='mt-12 px'>
-                <h1 className='text-4xl font-bold text-center text-slate-100 py-16 mx-auto 
-                rounded-xl current-number'>0</h1>
+                <h1 
+                    className='text-4xl font-bold text-center text-slate-100 py-16 mx-auto 
+                    rounded-xl current-number'
+                    ref={drop}>
+                    0
+                </h1>
             </section>
         </main>
     );
