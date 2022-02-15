33d4eb0033ed59fe1eff8a5aed44876bc1ce2526
Rafael Tivane <luislangabusiness@gmail.com>
2022-02-15 16:51:25 +0200

    adding react-dnd library

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './assets/css/index.css';
 import './assets/css/base.css';
 import App from './pages/App';
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 
 ReactDOM.render(
   <React.StrictMode>
+    <DndProvider backend={HTML5Backend}>
     <App />
+    </DndProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
